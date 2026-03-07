357933727a520560d407dd290ac3dec734a6f62d
Faizyab <notfaizyab@gmail.com>
2026-03-07 10:00:00 +0530

    Multithreading

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 import socket
 import time
+import threading
+from concurrent.futures import ThreadPoolExecutor
 
 target = input("Enter target IP: ")
 n = int(input("Enter the number of ports to scan: "))
-timereq = int(input("Enter the number of seconds you want to complete the scan in(Note: Entering a value < 0.5 seconds may miss open ports): "))
+open_ports = 0
+lock = threading.Lock()
 print("Scanning target:", target)
-
 start = time.perf_counter()
-for port in range(1, n+1):
-    print("scanning...", port)
+
+def scan(port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(timereq/n)
-    st=time.perf_counter()
+    s.settimeout(0.1)
     result = s.connect_ex((target, port))
-    en=time.perf_counter()
     if result == 0:
         print("Port", port, "is OPEN")
+        global open_ports
+        open_ports = open_ports
+        with lock:
+            open_ports+=1
     s.close()
+
+with ThreadPoolExecutor(max_workers=100) as executor:
+    executor.map(scan, range(1, n+1))
+    
 end = time.perf_counter()
-print("Time taken to scan " + str(n) + " ports:", round(end-start, 3))
+print("Scanned " + str(n) + " ports in:", round(end-start, 3)," seconds")
+print(open_ports, "Open ports found")
